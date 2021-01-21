33d0dd1669fff7b78c94e8d90112e2731d06e581
imanhamid <57411042+imanhamid@users.noreply.github.com>
2021-01-21 13:11:52 -0800

    fixed incorrect directory

--- globalancestry_sims/02_localancestry_driver.sh
+++ globalancestry_sims/02_localancestry_driver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sbatch -a 1-50 ~/home/CV_DuffySelection/SWIFr_sims/ancestry_whole-autosome.sh
-sbatch -a 1-50 ~/home/CV_DuffySelection/SWIFr_sims/ancestry_two-chromosome.sh
-sbatch -a 1-50 ~/home/CV_DuffySelection/SWIFr_sims/ancestry_ABC-accepted.sh
+sbatch -a 1-50 ~/home/CV_DuffySelection/globalancestry_sims/ancestry_whole-autosome.sh
+sbatch -a 1-50 ~/home/CV_DuffySelection/globalancestry_sims/ancestry_two-chromosome.sh
+sbatch -a 1-50 ~/home/CV_DuffySelection/globalancestry_sims/ancestry_ABC-accepted.sh
